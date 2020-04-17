4393b2a324b0e23162a21b797344177941618541
Raudel <raudel.developer@gmail.com>
2020-04-17 01:09:21 -0400

    working on group discriptors

--- fsa.c
+++ fsa.c
@@ -6,7 +6,7 @@
 
 //Function Prototypes
 void genFileSysInfo(struct ext2_super_block **);
-void indivGroupInfo(struct ext2_super_block **);
+void indivGroupInfo(struct ext2_super_block **, int );
 void rootDirEntries(struct ext2_super_block **);
 unsigned int calcBlockSizeBytes(struct ext2_super_block ***);
 unsigned int calcNumbInodeBlocksPGroup(struct ext2_super_block ***, unsigned int ); 
@@ -14,8 +14,7 @@ unsigned int calcNumbInodeBlocksPGroup(struct ext2_super_block ***, unsigned int
 int main(int argc, char *argv[]) {
 
   int rv, fd;
-  char *sb1 = NULL;
-  struct ext2_super_block *sb2 = NULL;
+  struct ext2_super_block *superBlock = NULL;
   
   if (argc != 2) {
     fprintf (stderr, "%s: Usage: ./sb disk_image_file\n", "sb");
@@ -34,102 +33,149 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   
-  sb2 = malloc(sizeof(struct ext2_super_block));
+  superBlock = malloc(sizeof(struct ext2_super_block));
 
-  if (sb2 == NULL) {
+  if (superBlock == NULL) {
     fprintf (stderr, "%s: Error in malloc\n", "sb");
     exit(1);
   }
   
   /*read the whole superblock and load into the ext2_super_block struct*/
   /*assumes the struct fields are laid out in the order they are defined*/
-  rv = read(fd, sb2, sizeof(struct ext2_super_block));
+  rv = read(fd, superBlock, sizeof(struct ext2_super_block));
   if (rv == -1) {
     perror("File read failed");
     exit(1);
   }
   if (rv == 1024) {
 
-    genFileSysInfo(&sb2);
-    indivGroupInfo(&sb2);
-    rootDirEntries(&sb2);
+    genFileSysInfo(&superBlock);
+    indivGroupInfo(&superBlock, fd);
+    rootDirEntries(&superBlock);
 
-    printf("Total Number of Inodes: %u\n", sb2->s_inodes_count);
+    printf("Total Number of Inodes: %u\n", superBlock->s_inodes_count);
 
   }    
   
-  free(sb1);
-  free(sb2);
+  free(superBlock);
   close(fd);
   
   return 0;
 }
 
 
-void genFileSysInfo(struct ext2_super_block **sb2) {
+void genFileSysInfo(struct ext2_super_block **superBlock) {
 
     printf("--General File System Information--\n");
 
-    printf("Block Size in Bytes: %u\n", calcBlockSizeBytes(&sb2));
-    printf("Total Number of Blocks: %u\n", (*sb2)->s_blocks_count);
-    printf("Disk Size in Bytes: %u\n", (*sb2)->s_blocks_count * calcBlockSizeBytes(&sb2));
+    printf("\tBlock Size in Bytes: %u\n", calcBlockSizeBytes(&superBlock));
+    printf("\tTotal Number of Blocks: %u\n", (*superBlock)->s_blocks_count);
+    printf("Disk Size in Bytes: %u\n", (*superBlock)->s_blocks_count * calcBlockSizeBytes(&superBlock));
 
     //NOTE: the last group will most probably not have this many blocks but all the groups 
     //execpt the last one will definitely have this many blocks
-    printf("Maximum Number of Blocks Per Group: %u\n", (*sb2)->s_blocks_per_group);
+    printf("\tMaximum Number of Blocks Per Group: %u\n", (*superBlock)->s_blocks_per_group);
 
-    printf("Inode Size in Bytes: %u\n", (*sb2)->s_inode_size);
-    printf("Number of Inodes Per Group: %u\n", (*sb2)->s_inodes_per_group);
-    printf("Number of Inode Blocks Per Group: %u\n", calcNumbInodeBlocksPGroup(&sb2, calcBlockSizeBytes(&sb2)));// COMEBACK
+    printf("\tInode Size in Bytes: %u\n", (*superBlock)->s_inode_size);
+    printf("\tNumber of Inodes Per Group: %u\n", (*superBlock)->s_inodes_per_group);
+    printf("\tNumber of Inode Blocks Per Group: %u\n", calcNumbInodeBlocksPGroup(&superBlock, calcBlockSizeBytes(&superBlock)));// COMEBACK
 
     //Assuming that the virtual disk provided has enough blocks in its last group to fit all of the
     //necessary data structures and some data blocks
-    printf("Number of Groups: %u\n", (*sb2)->s_blocks_count / (*sb2)->s_blocks_per_group);
+    printf("\tNumber of Groups: %u\n", (*superBlock)->s_blocks_count / (*superBlock)->s_blocks_per_group);
 
 }
 
-void indivGroupInfo(struct ext2_super_block **sb2) {
+void indivGroupInfo(struct ext2_super_block **superBlock, int fd) {
     
-    int numbGroups = (**sb2)->s_blocks_count / (**sb2)->s_blocks_per_group);
+    unsigned int numberOfGroups = 1 + ((**superBlock).s_blocks_count - 1) / (**superBlock).s_blocks_per_group;
+    //unsigned int sizeOfGroupDiscriptor = numberOfGroups * sizeof(struct ext2_group_desc);
+    unsigned int groupDiscrByteLocation = calcBlockSizeBytes(&superBlock); //At this point fd should alreadt be at 1024
+    struct ext2_group_desc *groupDescriptors = NULL;
+    unsigned int numberOfBlocksPerGroup = (**superBlock).s_blocks_per_group;
+    unsigned int firstDataBlockID = (**superBlock).s_first_data_block;
+    unsigned int totalNumbBlocks = (**superBlock).s_blocks_count;
+    unsigned int beginningID = firstDataBlockID;
+    unsigned int endingID = totalNumbBlocks < numberOfBlocksPerGroup ? totalNumbBlocks-1 : firstDataBlockID + numberOfBlocksPerGroup-1;
+    unsigned int remainingBlocks = totalNumbBlocks - numberOfBlocksPerGroup;
+    int rv = 0;
 
-    for(int i=0; i <= numbGroups; i++) {
-        printf("-Group %d-\n", i);
+    //moves the head to the position of the group discriptor block
+    lseek(fd, groupDiscrByteLocation, SEEK_SET);
 
-        printf("Block IDs: %u\n"); //COMEBACK
-        printf("Block Bitmap Block ID: %u\n"); //COMEBACK
-        printf("Inode Bitmap Block ID: %u\n"); //COMEBACK
-        printf("Inode Table Block ID: %u\n"); //COMEBACK
-        printf("Number of Free Blocks: %u\n"); //COMEBACK
-        printf("Number of Free Inodes: %u\n"); //COMEBACK
-        printf("Number of Dictionaries: %u\n"); //COMEBACK
-        printf("Free Block IDs: %u\n"); //COMEBACK
-        printf("Free Inode IDs: %u\n"); //COMEBACK
+    groupDescriptors = malloc(sizeof(struct ext2_group_desc));
+
+    if (groupDescriptors == NULL) {
+      fprintf (stderr, "%s: Error in malloc\n", "sb");
+      exit(1);
+    }
+    
+    rv = read(fd, groupDescriptors, sizeof(struct ext2_group_desc));
+    if (rv == -1) {
+      perror("File read failed");
+      exit(1);
+    }
+
+    if(rv == groupDiscrByteLocation) {
+
+      printf("\n--Individual group Information--\n");
+      for(int i = 0; i <= numberOfGroups; i++) {
+        printf("\t-Group %d-\n", i);
+        printf("\t\tBlock IDs: %u - %u\n", beginningID, endingID);
+        printf("\t\tBlock Bitmap Block ID: %u\n", groupDescriptors->bg_block_bitmap);
+        printf("\t\tInode Bitmap Block ID: %u\n", groupDescriptors->bg_inode_bitmap);
+        printf("\t\tInode Table Block ID: %u\n", groupDescriptors->bg_inode_table);
+        printf("\t\tNumber of Free Blocks: %u\n", groupDescriptors->bg_free_blocks_count);
+        printf("\t\tNumber of Free Inodes: %u\n", groupDescriptors->bg_free_inodes_count);
+        printf("\t\tNumber of Dictionaries: %u\n", groupDescriptors->bg_used_dirs_count);
+        
+        //printf("\t\tFree Block IDs: %u\n"); //COMEBACK
+        // printf("\t\tFree Inode IDs: %u\n"); //COMEBACK
+
+        if (remainingBlocks >= numberOfGroups) {
+
+          beginningID = endingID + 1;
+          endingID += numberOfBlocksPerGroup;
+          remainingBlocks -= numberOfBlocksPerGroup;
+
+        } else if(remainingBlocks > 0) {
+
+          beginningID = endingID + 1;
+          endingID += numberOfBlocksPerGroup;
+          remainingBlocks -= numberOfBlocksPerGroup;
+
+        } else {
+          printf("ERROR: There are more groups than there is remaining blocks!");
+        }
+
+    } else {
+      perror("Could not read discriptors block from disk!");
+    } 
+      
+
+
 
     }
 
-    printf("--Individual group Information--");
-
-    printf("Number of Free Inodes: %u\n", (*sb2)->s_free_inodes_count);
-
-
+    //printf("Number of Free Inodes: %u\n", (*superBlock)->s_free_inodes_count)
 
 }
 
-void rootDirEntries(struct ext2_super_block **sb2) {
+void rootDirEntries(struct ext2_super_block **superBlock) {
     printf("--Root Directory Entries--");
 
 
 }
 
-unsigned int calcBlockSizeBytes(struct ext2_super_block ***sb2) {
+unsigned int calcBlockSizeBytes(struct ext2_super_block ***superBlock) {
 
-    return 1024 << (**sb2)->s_log_block_size;
+    return 1024 << (**superBlock)->s_log_block_size;
 }
 
-unsigned int calcNumbInodeBlocksPGroup(struct ext2_super_block ***sb2, unsigned int blockSizeBytes) {
+unsigned int calcNumbInodeBlocksPGroup(struct ext2_super_block ***superBlock, unsigned int blockSizeBytes) {
 
-    unsigned int totalNumbBlocks = (**sb2)->s_blocks_count;
-    unsigned int inodeSizeBytes = (**sb2)->s_inode_size;
+    unsigned int totalNumbBlocks = (**superBlock)->s_blocks_count;
+    unsigned int inodeSizeBytes = (**superBlock)->s_inode_size;
 
     return (totalNumbBlocks * inodeSizeBytes) / blockSizeBytes;
 }
